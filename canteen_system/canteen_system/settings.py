70600a102463ca7cab1b9551a5e0ec1832d2064b
IshanBawne <ishanb20@iitk.ac.in>
2022-03-13 23:08:21 +0530

    Added app configs

--- canteen_system/canteen_system/settings.py
+++ canteen_system/canteen_system/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'automation_system.apps.AutomationSystemConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
