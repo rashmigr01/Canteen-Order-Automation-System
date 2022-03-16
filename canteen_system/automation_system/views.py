b03a68fd8bdc43aac5bea02365ed53344a4950ad
IshanBawne <ishanb20@iitk.ac.in>
2022-03-16 17:00:32 +0530

    More Changes on Authentication

--- canteen_system/automation_system/views.py
+++ canteen_system/automation_system/views.py
@@ -9,9 +9,18 @@ def index(request):
 
 def register(request):
     if request.method == "POST":
-        user = User.objects.create(request.POST["usern"], request.POST["email"], request.POST["passw"])
+        user = User( username = request.POST["usern"], email = request.POST["email"], password= request.POST["passw"])
         user.save()
-        userEx = UserExt.objects.create(user,int(request.POST["roll"]), int(request.POST["phone"]),request.POST["isStaff"])
+
+        print(int(request.POST["roll"]))
+
+        userEx = UserExt(user = user)
+        print(userEx.__dict__)
         userEx.save()
+        userEx.roll = int(request.POST["roll"])
+        userEx.phone = int(request.POST["phone"])
+        userEx.isStaff = True if request.POST["isStaff"] == "True" else False
+        userEx.save()
+        print(userEx.__dict__)
         print("This ran")
     return render(request, 'home/register.html')
