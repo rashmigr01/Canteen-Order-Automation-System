e55104476964249defaff9d5230437d6161190d3
jhaansireddy <jrgjhaansireddy@gmail.com>
2022-03-21 18:03:55 +0530

    :)

--- canteen_system/automation_system/views.py
+++ canteen_system/automation_system/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 
 def register(request):
+    print(request)
     if request.user.is_authenticated:
         return HttpResponseRedirect(reverse("auto:home"))
     if request.method == "POST":
@@ -30,6 +31,7 @@ def login_user(request):
         
         username = request.POST['username']
         password = request.POST['password']
+        #print(username,' ',password)
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request,user)
